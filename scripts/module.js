8e11694d65a3268d291c5b3e4faa0109c9e313ec
Sierra Leanne Kristiaan <Railec@users.noreply.github.com>
2023-01-22 22:25:46 +0100

    Organised a bunch of text into translation entries

--- scripts/module.js
+++ scripts/module.js
@@ -2,15 +2,24 @@ import "./data/FateXAddon.js";
 import "./data/ActorInventory.js";
 import "./ui/ActorInventoryTab.js";
 
+//Load templates on system init.
 Hooks.once('init', async function () {
 	loadTemplates([
 		FateXAddon.Templates.ActorInventory
 	]);
 });
 
+//Hook into the renderActorSheet to inject the inventory and spellbook tabs.
 Hooks.on('renderActorSheet', async function (app, html, data) {
 	if (app.object && app.object.name && app.object.flags) {
 		console.log(`FateX-Addon | Initializing inventory tab for ${app.object.name}`);
 		new ActorInventoryTab(app, app.object).render();
+	}
+});
+
+//Hook into the preUpdateActor to intercept render calls for updates to the inventory flags.
+Hooks.on('preUpdateActor', async function (fateActor, data, options, userId) {
+	if (data.flags && data.flags.fatex.inventory) {
+		options.render = false;
 	}
 });
--- scripts/ui/ActorInventoryTab.js
+++ scripts/ui/ActorInventoryTab.js
@@ -1,5 +1,10 @@
 /**
- * The following class defines the Inventory tab for the FateX ActorSheet.
+ * This class defines and controls the Inventory Tab.
+ * It is intended and should only ever be used on a FateX actor sheet.
+ * 
+ * The inventory tab is injected into the actor sheet's html.
+ * The tab provides controls for manipulating a FateX actor's "inventory",
+ * which is a concept that Fate by default doesn't have and is borrowed in a lot of ways from DnD and similar.
  */
 class ActorInventoryTab {
 	#application;
@@ -29,7 +34,7 @@ class ActorInventoryTab {
 			`);
 		} else {
 			this.#application.element.find(".fatex-desk__tabs .fatex-js-tabs-navigation").append(`
-				<a class="fatex-tabs-navigation__item" data-tab="inventory">Inventory</a>
+				<a class="fatex-tabs-navigation__item" data-tab="inventory">${game.i18n.localize("FATEX-ADDON.Inventory")}</a>
 			`);
 
 			this.#application.element.find(".fatex-desk__tabs .fatex-js-tab-content").append(`
@@ -51,13 +56,13 @@ class ActorInventoryTab {
 		const parent = $(e.currentTarget).parent(".fatex-actions");
 		if (parent.length > 0) {
 			Dialog.confirm({
-				title: "Delete item container",
-				content: `<p>Delete item container: ${parent[0].dataset.container}?</p>`,
+				title: game.i18n.localize("FATEX-ADDON.INVENTORY.DIALOG.CONTAINER.DELETE.Title"),
+				content: `<p>${game.i18n.localize("FATEX-ADDON.INVENTORY.DIALOG.CONTAINER.DELETE.Content")}: ${parent[0].dataset.container}?</p>`,
 				yes: () => {
 					if (actorInventory.removeContainer(parent[0].dataset.container)) {
 						this.render();
 					} else {
-						ui.notifications.error(`Cannot remove the item container: ${parent[0].dataset.container}.`);
+						ui.notifications.error(`${game.i18n.localize("FATEX-ADDON.INVENTORY.DIALOG.CONTAINER.DELETE.Fail")}: ${parent[0].dataset.container}.`);
 					}
 				},
 				no: () => { },
@@ -87,28 +92,28 @@ class ActorInventoryTab {
 		e.preventDefault();
 
 		new Dialog({
-			title: "Create new item container",
-			content: `<p><label>Container name:</label><br/><input id="containerName" type="text"/></p>`,
+			title: game.i18n.localize("FATEX-ADDON.INVENTORY.DIALOG.CONTAINER.ADD.Title"),
+			content: `<p><label>${game.i18n.localize("FATEX-ADDON.INVENTORY.DIALOG.CONTAINER.ADD.Content")}:</label><br/><input id="containerName" type="text"/></p>`,
 			buttons: {
-				cancelButton: {
-					label: "Cancel",
-					callback: (html) => { },
-					icon: `<i class="fas fa-cancel"></i>`
-				},
 				createButton: {
-					label: "Create",
+					label: game.i18n.localize("Save"),
 					callback: (html) => {
-						const value = html.find("input#containerName").val();
+						const value = html.find("input#containerName").val(game.i18n.localize("FATEX-ADDON.INVENTORY.DIALOG.CONTAINER.ADD.FailEmptyName"));
 
 						if (value.trim() === "") {
-							ui.notifications.error(`Cannot create a new item container without a name.`);
+							ui.notifications.error();
 						} else if (actorInventory.addContainer(value, 0)) {
 							this.render();
 						} else {
-							ui.notifications.error(`Cannot create a new item container.`);
+							ui.notifications.error(`${game.i18n.localize("FATEX-ADDON.INVENTORY.DIALOG.CONTAINER.ADD.Fail")}: ${value}.`);
 						}
 					},
 					icon: `<i class="fas fa-check"></i>`
+				},
+				cancelButton: {
+					label: game.i18n.localize("Cancel"),
+					callback: (html) => { },
+					icon: `<i class="fas fa-cancel"></i>`
 				}
 			},
 			default: "cancelButton"
